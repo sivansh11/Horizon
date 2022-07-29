fc4e1d40d08666c720818217aab1dd0f2c60cc87
sivansh <sivanshgupta2002@gmail.com>
2022-07-30 01:53:27 +0530

    added a comment

--- shadercompiler.py
+++ shadercompiler.py
@@ -1,10 +1,15 @@
 import os
 import platform
 
-if platform.system() == 'Windows':
+
+# use your path for glslc here
+# example shader_compiler = 'my_path_to_glslc'
+shader_compiler = ''
+if platform.system() == 'Windows' and shader_compiler == '':
     shader_compiler = 'C:\\programming\\shaderc\\bin\\glslc'
-if platform.system() == 'Linux':
+if platform.system() == 'Linux' and shader_compiler == '':
     shader_compiler = 'glslc'
+
 
 if not os.path.exists("build"):
     print("Please run cmake!")
